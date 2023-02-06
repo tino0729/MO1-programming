872892fa5e256bf395984180627ef77f200fa9b2
tino0729 <cderosa1@ivytech.edu>
2023-02-06 10:34:05 -0600

    mo3 lab

---
+++ Module 2 loops and conditionals/class Vehicle:.py
@@ -0,0 +1,15 @@
+class Vehicle:
+    def __init__(self, vehicle_type: str) -> None:
+        self.vehicle_type: str = vehicle_type
+
+class Automobile(Vehicle):
+    def __init__(self, vehicle_type: str) -> None:
+        super().__init__(vehicle_type)
+        self.year: int = 0
+        self.make: str = ''
+        self.medel: str = ''
+        self.roof: str = 'solid'
+
+car = Automobile(vehicle_type='car') 
+car.year = int(input('Enter the car year:'))
+print(f"Vehicle type: {car.vehicle_type}")
